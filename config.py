2df941fe8a1cff8aabaf1891fa9e9ecfe09c1649
Source crystal <132131183+NO1BROS@users.noreply.github.com>
2023-05-04 14:25:10 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,16 +17,16 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "cr")
 
 OWNER_ID = list(
-  map(int, getenv("OWNER_ID", "").split())) + [1715562844]
+  map(int, getenv("OWNER_ID", "").split())) + [5676384368]
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Tompriv/Tom")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/NO1BROS/KING")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/B700B")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NettChat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NO1BROS")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ppblb")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
